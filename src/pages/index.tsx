4f0096aacb4f3de9f2cac9edf1276fac95a1e707
Dominik Hofer <hfrdmnk@gmail.com>
2023-04-17 19:48:20 +0200

    Next image tag, styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
 
-import { RouterOutputs, api } from "~/utils/api";
+import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
 import Image from "next/image";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -12,10 +18,16 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <img
+      <Image
         src={user.profileImageUrl}
-        alt="Profile image"
+        alt={
+          user.username
+            ? `@${user.username}'s profile picture`
+            : "User profile picture"
+        }
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <input
         placeholder="Type some emojis ✌️"
@@ -25,6 +37,7 @@ const CreatePostWizard = () => {
   );
 };
 
+// TODO: Figure out how this works
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 
 const PostView = (props: PostWithUser) => {
@@ -32,16 +45,20 @@ const PostView = (props: PostWithUser) => {
 
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <img
+      <Image
         src={author.profileImageUrl}
-        alt="Profile image"
+        alt={`@${author.username}'s profile picture`}
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <div className="flex flex-col">
         <div className="flex gap-2 text-slate-300">
           <span>{`@${author.username}`}</span>
           <span className="font-thin">· </span>
-          <span className="font-thin">{`1 hour ago`}</span>
+          <span className="font-thin">{`${dayjs(
+            post.createdAt
+          ).fromNow()}`}</span>
         </div>
         <span>{post.content}</span>
       </div>
